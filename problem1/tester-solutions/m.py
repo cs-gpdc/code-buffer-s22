1721d17225dcda13eb99f6a3a4fa4f5a271950aa
sspathare97 <sspathare97@gmail.com>
2022-04-22 23:24:32 -0600

    minor formatting

--- problem1/tester-solutions/m.py
+++ problem1/tester-solutions/m.py
@@ -1,17 +1,7 @@
-# Python program to check whether input degree
-# sequence is graph or tree
 def check(degree, n):
-     
-    # Find sum of all degrees
     deg_sum = sum(degree)
+    return (2*(n-1) == deg_sum)
      
-    # It is tree if sum is equal to 2(n-1)
-    if (2*(n-1) == deg_sum):
-        return True
-    else:
-        return False
-     
-#main
 n = int(input())
 degree = [int(item) for item in input().split()]
 if (check(degree, n)):
